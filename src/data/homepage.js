ffcdfb1bebd41bfb56fee7daaac3a9e3ff43edac
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-26 20:15:08 +0530

    feat - Home Page Created

---
+++ src/data/homepage.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    title: "Why use Archivio ?",
+    description: [
+      `
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod maiores, aperiam veniam ea illo officia dolore aliquam at iusto esse consequuntur deleniti vero voluptates illum eveniet labore a pariatur nostrum.
+    `,
+      `
+        It is efficient and provides much more control over the data. Moreover one can choose what type of data access can be given to a particular person to maintain proper access protocols.
+    `,
+    ],
+  },
+  {
+    title: "Archivio Welcomes you!",
+    description: [
+      `
+        Archivio is a Library Management Tool which can help you to manage books and renters inside your own library and helps tracking the book status and the available books
+    `,
+    ],
+  },
+  {
+    title: "Already a Librarian ?",
+    description: [
+      `
+        Hello there, Glad to see you back here!! If you are finding the way to login into your account, look no further as you can login through clicking the button down here.
+    `,
+    ],
+  },
+];
+
+export const imageArray = [
+  "https://images.unsplash.com/photo-1532012197267-da84d127e765?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+  "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+  "https://images.unsplash.com/photo-1573592371950-348a8f1d9f38?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { data, imageArray } from "../data/homepage";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    // Container for centering the div
+    <div className="flex flex-col items-center justify-center py-12 gap-8">
+      {/* Grid Container */}
+      <div className="grid grid-cols-2 gap-20 place-items-center w-4/5">
+        <HomePageImage src={imageArray[0]} />
+
+        <HomePageContentItem data={data[0]} />
+
+        <HomePageContentItem
+          data={data[1]}
+          buttonTitle={"Get Started with Archivio"}
+          onClick={() => {}}
+        />
+
+        <HomePageImage src={imageArray[1]} />
+        <HomePageImage src={imageArray[2]} />
+
+        <HomePageContentItem
+          data={data[2]}
+          buttonTitle={"Login"}
+          onClick={() => {}}
+        />
+      </div>
+    </div>
+  );
+};
+
+function HomePageImage({ src }) {
+  return (
+    <img
+      className="h-96 w-full object-cover rounded-xl"
+      src={src}
+      alt="Library Image"
+    />
+  );
+}
+
+const HomePageContentItem = ({ data, buttonTitle, onClick }) => {
+  const { title, description } = data;
+  return (
+    <div className="flex flex-col gap-6 rounded-3xl p-4">
+      <h2 className="font-bold text-3xl">{title}</h2>
+
+      {description.map((text, index) => (
+        <p key={index} className="opacity-80">
+          {text}
+        </p>
+      ))}
+
+      {buttonTitle && onClick && (
+        <button
+          onClick={onClick}
+          className="p-3 px-12 w-fit rounded-xl font-bold bg-cardVariant cursor-pointer hover:bg-primary transition-all duration-300 active:opacity-30"
+        >
+          {buttonTitle}
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
