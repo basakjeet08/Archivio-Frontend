318a6002b69ab0a1e7a10454fa3dfb4c11972706
Anirban Basak <jeetbasak2002@gmail.com>
2025-01-26 18:36:01 +0530

    feat - Minor Changes and added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Testing New Tailwind</h1>;
+  return <HomePage />;
 }
 
 export default App;
